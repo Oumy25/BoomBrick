dd38f79a8af8b6c026891336a57066160b104e9a
oumyndiaye <oumy.ndiaye@edu.esiee.fr>
2024-11-08 14:16:45 +0100

    modif de test

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.helloworld;
 
+import static android.view.WindowManager.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
- // vois tu mon message
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+    public void startgame(View view ){
+        GameView gameView = new GameView(this);
+        setContentView(gameView);
     }
 }
